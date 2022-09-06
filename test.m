3a24716e0165598d1ee581a20f973063179cdfd9
Randy <randyosbaldo@gmail.com>
2022-09-06 14:25:55 -0500

    fix: call to differential_evolution (new parameters)

--- test.m
+++ test.m
@@ -9,9 +9,11 @@ population_size = 10; % population_size >= 4
 diff_weight = 1.5;     % diff_weight \in [0, 2]
 cross_prob = 0.5;    % cross_prob \in [0,1]
 
+%% TESTING FUNCTIONS
+
 % get boundaries of function to test
 [lower_b, upper_b] = boundaries_rosenbrock(dimension);
 % initial population: each "entry vector" came from a Unif[lower_b, upper_b]
 population = init_random(dimension, population_size, lower_b, upper_b);
 % search solution
-[optimal_agent, optimal_value, f_time] = differential_evolution(population, @f_rosenbrock, n_iterations, diff_weight, cross_prob);
+[optimal_agent, optimal_value, f_time] = differential_evolution(population, @f_rosenbrock, n_iterations, diff_weight, cross_prob, lower_b, upper_b);
