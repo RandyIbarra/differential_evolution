5defbe25829932a58ae8f50368bbb2f88a0b0b8f
Randy <randyosbaldo@gmail.com>
2022-09-06 14:08:54 -0500

    chore: function to get a random (uniform) number in (lower_b, upper_b)

---
+++ utils/get_uniform.m
@@ -0,0 +1,14 @@
+% returns a colum vector or a single scalar with uniform distribution in (l_b, u_b)
+function random_number = get_uniform(lower_b, upper_b)
+    if length(lower_b) ~= length(upper_b)
+        fprintf("length is not the same \n");
+    end
+    if size(lower_b, 1) == 1
+        lower_b = lower_b';
+        upper_b = upper_b';
+    end
+    disp(rand(length(upper_b), 1));
+    disp(size(rand(length(upper_b), 1)));
+    disp(size(upper_b));
+    random_number = lower_b+(upper_b - lower_b).*rand(length(upper_b), 1);
+end
